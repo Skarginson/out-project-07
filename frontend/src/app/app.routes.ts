5f3df49d651a64960784e4c6687388ce6993eda6
Skarginson <eldaraes@gmail.com>
2026-03-17 23:58:53 +0100

    feature: frontend list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Gallery } from './features/gallery/gallery';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'gallery', pathMatch: 'full' },
+  { path: 'gallery', component: Gallery },
+];
