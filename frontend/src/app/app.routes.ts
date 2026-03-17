7054240be77d05042789d26c62161f4470b81b7e
Skarginson <eldaraes@gmail.com>
2026-03-18 00:14:37 +0100

    fix to PR #25 as requested. Done myself because Alexandre was involved in a drug related shoutout.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { StudentList } from './pages/student-list/student-list';
 import { StudentDetail } from './pages/student-detail/student-detail';
 import { StudentNew } from './pages/student-new/student-new';
 import { StudentEdit } from './pages/student-edit/student-edit';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: StudentList },
   { path: 'student/new', component: StudentNew },
   { path: 'student/:id', component: StudentDetail },
   { path: 'student/:id/edit', component: StudentEdit },
+  { path: '**', component: NotFound },
 ];
